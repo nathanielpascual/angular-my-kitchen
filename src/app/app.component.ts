37d0e1b12a4418fdeea8cd255dcb0d7f95089237
nathanielpascual <nath_pascual@yahoo.com>
2019-01-06 22:39:30 +0800

    add authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // loadedFeature = 'recipe';
 
   // onNavigate(feature: string) {
   //   this.loadedFeature = feature;
   // }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCHGXuSKZ2WW7PCvrOwEkj5qEKMgTxNPnA",
+      authDomain: "recipe-book-7037c.firebaseapp.com"
+    });
+  }
 }
