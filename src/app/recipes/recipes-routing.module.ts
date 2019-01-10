134bcb609ad6e7bda98866c8ad0fca1908a2669f
nathanielpascual <nath_pascual@yahoo.com>
2019-01-10 23:00:11 +0800

    Added home and lazy loading for Recipes

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -7,7 +7,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { AuthGuard } from '../auth/auth-guard.service';
 
 const recipeRoutes: Routes = [
-    { path: 'recipes', component: RecipesComponent,children:[
+    { path: '', component: RecipesComponent,children:[
         {path:'', component:RecipeStartComponent},
         {path: 'new',component:RecipeEditComponent,canActivate:[AuthGuard]},
         {path: ':id',component:RecipeDetailComponent},    
