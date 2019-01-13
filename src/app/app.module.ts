f8d390fb87751767eb62334ee1341f72a8db33b6
nathanielpascual <nath_pascual@yahoo.com>
2019-01-13 16:17:54 +0800

    replaced http with httpmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+///import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,7 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
             BrowserModule,
-            HttpModule,
+            ///HttpModule,
+            HttpClientModule,
             AuthModule,
             ShoppingListModule,
             AppRoutingModule,
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/shared/recipe.service';
 import { Recipe} from '../recipes/recipe.model';
 import 'rxjs/Rx';
 import { AuthService } from '../auth/auth.service';
 
+
 @Injectable()
 export class DataStorageService {
-    constructor (private http: Http,
+    constructor (private httpClient: HttpClient,
                  private recipeService: RecipeService,
                  private authService : AuthService) {}
 
     storeRecipes() {
         const token = this.authService.getToken();
-        return this.http.put(`https://recipe-book-7037c.firebaseio.com/recipes.json?auth=${token}`,this.recipeService.getRecipes());
+        return this.httpClient.put(`https://recipe-book-7037c.firebaseio.com/recipes.json?auth=${token}`,this.recipeService.getRecipes());
     }
 
     getRecipes() {
         const token = this.authService.getToken();
-        return this.http.get(`https://recipe-book-7037c.firebaseio.com/recipes.json?auth=${token}`)
+        //return this.httpClient.get<Recipe[]>(`https://recipe-book-7037c.firebaseio.com/recipes.json?auth=${token}`)
+        return this.httpClient.get<Recipe[]>(`https://recipe-book-7037c.firebaseio.com/recipes.json?auth=${token}`,
+        {
+            observe:'body',
+            responseType:'json'
+        })
                    .map(
-                       (response : Response)=> {
-                        const recipes = response.json();
+                       (recipes)=> {
+                        //const recipes = response.json();
                         for(let recipe of recipes)
                         {
                             if(!recipe['ingredients']){
