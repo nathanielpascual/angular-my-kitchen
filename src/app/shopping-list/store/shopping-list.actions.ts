2d06088400d4a389945723eb87533a63ae2abb91
nathanielpascual <nath_pascual@yahoo.com>
2019-01-16 00:25:09 +0800

    Added Update and Delete Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action{
     readonly type = ADD_INGREDIENT;
@@ -18,5 +20,21 @@ export class AddIngredients implements Action{
     
 }
 
+export class UpdateIngredient implements Action{
+    readonly type = UPDATE_INGREDIENT;
+    
+    constructor(public payload : Ingredient) {}
+    
+}
+
+export class DeleteIngredient implements Action{
+    readonly type = DELETE_INGREDIENT;
+    
+    constructor(public payload : Ingredient) {}
+    
+}
+
 export type ShoppingListActions = AddIngredient |
-                                   AddIngredients;
+                                   AddIngredients|
+                                   UpdateIngredient |
+                                   DeleteIngredient;
