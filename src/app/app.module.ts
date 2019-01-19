509c6be2f135a735035127c2a89cd28047fa16a9
nathanielpascual <nath_pascual@yahoo.com>
2019-01-19 22:54:02 +0800

    add app wide reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
+import {reducers} from './store/app.reducers';
 
 
 @NgModule({
@@ -27,7 +27,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
             AppRoutingModule,
             SharedModule,
             CoreModule,
-            StoreModule.forRoot({shoppingList: shoppingListReducer})
+            StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
