790e6fd3b4f09f28610779167bf866bf46f02718
nathanielpascual <nath_pascual@yahoo.com>
2019-01-21 14:01:01 +0800

    added router-store and store-dev-tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { StoreRouterConnectingModule} from '@ngrx/router-store';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
@@ -14,7 +15,7 @@ import { StoreModule } from '@ngrx/store';
 import {reducers} from './store/app.reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
-
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,9 @@ import { AuthEffects } from './auth/store/auth.effects';
             SharedModule,
             CoreModule,
             StoreModule.forRoot(reducers),
-            EffectsModule.forRoot([AuthEffects])
+            EffectsModule.forRoot([AuthEffects]),
+            StoreRouterConnectingModule,
+            !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   bootstrap: [AppComponent]
 })
