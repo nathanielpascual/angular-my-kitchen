00d964661ab47ed7255a3ed4b96495e798b4dd3f
nathanielpascual <nath_pascual@yahoo.com>
2019-01-24 17:05:44 +0800

    replaced services with reduces in recipelist

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,38 +1,28 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import {Recipe} from '../recipe.model';
-import { RecipeService } from '../shared/recipe.service';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+import * as fromRecipe from '../store/recipe.reducers';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit,OnDestroy {
+export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[];
-  subscription : Subscription;
-  constructor(private recipeService: RecipeService,
-             private router: Router,
-             private route:ActivatedRoute) {
+  recipeState: Observable<fromRecipe.State>;
 
+  constructor(private router: Router,
+             private route:ActivatedRoute,
+             private store:Store<fromRecipe.FeatureState>) {
    }
 
   ngOnInit() {
-    this.subscription= this.recipeService.recipeChanged
-        .subscribe(
-          (recipes:Recipe[])=>{
-            this.recipes = recipes;
-          });
-    this.recipes = this.recipeService.getRecipes();
+     this.recipeState = this.store.select('recipes');
   }
 
   onNewRecipe() {
     this.router.navigate(['new'],{relativeTo: this.route});
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,7 +1,9 @@
 import { Recipe } from '../recipe.model';
 import {Ingredient } from '../../shared/ingredient.model';
 import * as RecipeActions from './recipe.actions';
-export interface FeatureState {
+import * as fromApp from '../../store/app.reducers';
+
+export interface FeatureState extends fromApp.AppState {
     recipes:State
 }
 
